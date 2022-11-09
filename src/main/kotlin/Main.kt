a6b7395adf4c6e98979a2ae3e74d036b06dd337f
krzysko <krzysko@ymail.com>
2022-11-09 21:51:44 +0100

    Refactor
    Group instructions in functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,32 @@
 import java.util.*
 
+val scanner = Scanner(System.`in`)
+
 fun main(args: Array<String>) {
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    // println("Program arguments: ${args.joinToString()}")
 
-    val scanner = Scanner(System.`in`)
+    greet("Krzys", "2022")
+    remindName()
+    guessAge()
+    count()
+    test()
+    end()
+}
 
-    println("Hello! My name is Aid.")
-    println("I was created in 2020.")
+fun greet(assistantName: String, birthYear: String) {
+    println("Hello! My name is ${assistantName}.")
+    println("I was created in ${birthYear}.")
     println("Please, remind me your name.")
+}
 
+fun remindName() {
     // reading a name
     val yourName = scanner.nextLine()
     println("What a great name you have, $yourName!")
-    println("Let me guess your age.\n" +
-            "Enter remainders of dividing your age by 3, 5 and 7.")
+}
+
+fun guessAge() {
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
 
     val remainder3 = readln().toInt()
     val remainder5 = readln().toInt()
@@ -25,17 +36,25 @@ fun main(args: Array<String>) {
     val yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 
     println("Your age is ${yourAge}; that's a good time to start programming!")
-    println("Now I will prove to you that I can count to any number you want.")
+}
 
+fun count() {
+    println("Now I will prove to you that I can count to any number you want.")
     // read a number and count to it here
     val counter = scanner.nextInt()
     for (i in 0..counter) {
         println("$i!")
     }
-    println("Completed, have a nice day!")
-
-    println(isGreater(3,4,1,2))
-
+}
+fun test() {
+    TODO("Not yet implemented")
 }
 
+fun end() {
+    println("Completed, have a nice day!")
+}
+
+/*
 fun isGreater(a: Int, b: Int, c: Int, d: Int,) = a + b > c + d
+println(isGreater(3,4,1,2))
+ */
